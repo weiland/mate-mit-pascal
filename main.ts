0713012da628e90fbd6271051cbe00dd42becf03
Pascal Weiland <weiland@users.noreply.github.com>
2023-07-31 18:20:13 +0200

    reformat workflow

--- main.ts
+++ main.ts
@@ -1,27 +1,28 @@
-import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import data from "./data.json" assert { type: "json" };
+import { Application, Router } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
+import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
+import data from './data.json' assert { type: 'json' };
 
 const router = new Router();
 router
-  // .get("/", (context) => {
-  //   context.response.body = "Welcome to dinosaur API!";
-  // })
-  .get("/api", (context) => {
-    context.response.body = data;
-  })
-  .get("/api/:dinosaur", (context) => {
-    if (context?.params?.dinosaur) {
-      const found = data.find((item) =>
-        item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-      );
-      if (found) {
-        context.response.body = found;
-      } else {
-        context.response.body = "No dinosaurs found.";
-      }
-    }
-  });
+	// .get("/", (context) => {
+	//   context.response.body = "Welcome to dinosaur API!";
+	// })
+	.get('/api', (context) => {
+		context.response.body = data;
+	})
+	.get('/api/:dinosaur', (context) => {
+		if (context?.params?.dinosaur) {
+			const found = data.find((item) =>
+				item.name.toLowerCase() ===
+					context.params.dinosaur.toLowerCase()
+			);
+			if (found) {
+				context.response.body = found;
+			} else {
+				context.response.body = 'No dinosaurs found.';
+			}
+		}
+	});
 
 const app = new Application();
 app.use(oakCors()); // Enable CORS for All Routes
@@ -30,16 +31,16 @@ app.use(router.allowedMethods());
 
 // static content
 app.use(async (context, next) => {
-    const root = `${Deno.cwd()}/dist`
-    try {
-        await context.send({ 
-                        root ,
-                        index: "index.html",
-                })
-    } catch {
-        next()
-    }
-})
+	const root = `${Deno.cwd()}/dist`;
+	try {
+		await context.send({
+			root,
+			index: 'index.html',
+		});
+	} catch {
+		next();
+	}
+});
 
 // TODO: page not found
 
