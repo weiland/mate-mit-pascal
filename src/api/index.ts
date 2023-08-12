3ab3b8bc50be5d692f1dfc26accfdd192c23b100
Pascal Weiland <weiland@users.noreply.github.com>
2023-08-12 23:18:19 +0200

    Implement basic login

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,7 @@ import {
 	helpers,
 	Router,
 } from 'https://deno.land/x/oak@v12.4.0/mod.ts';
+import { load } from 'https://deno.land/std/dotenv/mod.ts';
 import {
 	cancelMeeting,
 	createMeeting,
@@ -14,9 +15,20 @@ import {
 } from './db.ts';
 const { getQuery } = helpers;
 
+const env = await load();
+const TOKEN = Deno.env.get('TOKEN') ?? env['TOKEN'];
+
+const isLoggedIn = async (context: Context): Promise<boolean> =>
+	TOKEN === await context.cookies.get('token');
+
 export const apiRouter = new Router();
 apiRouter
 	.get('/api/meetings', async (context: Context) => {
+		if (!await isLoggedIn(context)) {
+			context.response.status = 403;
+			context.response.body = { error: 'Not authorized.' };
+			return;
+		}
 		context.response.body = await getAllMeetings();
 	})
 	.get('/api/meetings/:id', async (context: Context) => {
@@ -31,6 +43,10 @@ apiRouter
 	})
 	.put('/api/meetings/:id/drunk', async (context: Context) => {
 		const { id } = getQuery(context, { mergeParams: true });
+		if (!await isLoggedIn(context)) {
+			context.response.status = 403;
+			return;
+		}
 		const cancelled = await finishMeeting(id);
 		if (cancelled) {
 			context.response.body = { status: 'ok' };
@@ -67,25 +83,20 @@ apiRouter
 		const body = await createMeeting(meeting);
 		context.response.body = body;
 	})
-	.post('/api/login', async (context: Context, next) => {
-		console.log('login')
-		const PASSWORD = import.meta.env.VITE_PASSWORD ??
-			Deno.env.get('PASSWORD');
-		const TOKEN = import.meta.env.VITE_TOKEN ?? Deno.env.get('TOKEN');
+	.post('/api/login', async (context: Context) => {
+		const PASSWORD = Deno.env.get('PASSWORD') ?? env.PASSWORD;
 
 		if (!PASSWORD || !TOKEN) {
 			console.error('There are missing credentials.');
 			context.response.status = 500;
 			context.response.body = { error: 'There are missing credentials.' };
-			next();
 			return;
 		}
 
-		const form = context.request.body({ type: 'form-data' });
-		console.log(form)
-		const data = await form.value.read();
-		const password = data.fields.password;
-		const loggedIn = password === PASSWORD;
+		const form = context.request.body();
+		const data = await form.value;
+		const loggedIn = data.has('password') &&
+			data.get('password') === PASSWORD;
 		if (loggedIn) {
 			await context.cookies.set('token', TOKEN);
 			// context.response.headers = {
@@ -96,7 +107,10 @@ apiRouter
 			? { status: 'ok' }
 			: { error: 'invalid password' };
 		context.response.body = body;
-		next();
+	})
+	.get('/api/logout', async (context: Context) => {
+		context.response.body = '';
+		await context.cookies.delete('token');
 	})
 	.all('/api/:rest*', (context: Context) => {
 		context.response.status = 404;
